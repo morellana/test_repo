bc419cd54b30cc4eb24e137b6b1b1c76a9e92bfc
morellana <mauricio-09-08@hotmail.com>
2018-05-25 21:37:27 -0300

    Adding person class

---
+++ test-artifact/src/main/java/com/example/Person.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Person {
+
+	private String firstname;
+	private String lastname;
+	
+	public Person (String firstname, String lastname) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+	}
+	
+}
